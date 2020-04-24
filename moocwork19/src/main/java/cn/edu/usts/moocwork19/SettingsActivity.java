421a701bee67ba354e6b93ef2b51e0bf00a35792
unknown <1161648282@qq.com>
2020-04-24 17:53:51 +0800

    mooc19

---
+++ moocwork19/src/main/java/cn/edu/usts/moocwork19/SettingsActivity.java
@@ -0,0 +1,13 @@
+package cn.edu.usts.moocwork19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.settings);
+    }
+}
